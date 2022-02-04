d8138cf4b2f2e916eb220118b0cd7b079b690047
Lennard Haskamp <haskamp.lennard@gmail.com>
2022-02-04 13:26:11 +0100

    chore: card main content outsourced/refactored // Image of Card Media needs fix

---
+++ src/molecules/card-main-content/index.jsx
@@ -0,0 +1,24 @@
+import { CardActionArea, CardContent, CardMedia, Typography } from "@mui/material";
+import { LocationOn, Verified } from "@mui/icons-material";
+import React from "react";
+
+const CardMainContent = ({ card }) => {
+	return (
+		<CardActionArea style={{ position: "relative" }}>
+			{/*<CardMedia component="img" image={card.photo} alt={`Kite-Instructor ${card.name}`} />*/}
+			<CardContent>
+				<Typography variant="h4">{card.name}</Typography>
+				<Typography variant="h6">
+					<LocationOn />
+					{card.location}
+				</Typography>
+				<Typography variant="h6">
+					<Verified />
+					{card.license}
+				</Typography>
+			</CardContent>
+		</CardActionArea>
+	);
+};
+
+export default CardMainContent;
