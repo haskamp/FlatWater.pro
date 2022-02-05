542bfc87b0893ef7c6392af036cd7f29a81dd283
Lennard Haskamp <haskamp.lennard@gmail.com>
2022-02-05 13:39:44 +0100

    chore: card extended infos refactor

---
+++ src/molecules/card-availabilities/index.jsx
@@ -0,0 +1,30 @@
+import { Box, Button, ButtonGroup } from "@mui/material";
+import React from "react";
+import useStore from "../../ions/store";
+
+const weekdays = ["man", "tue", "wed", "thu", "fri", "sat", "sun"];
+
+const CardAvailabilities = ({ index }) => {
+	const cards = useStore(state => state.cards);
+	return (
+		<ButtonGroup variant="outline" sx={{ width: "100%" }}>
+			{weekdays.map((day, i) => {
+				const cardAvailabilities = cards[index].availabilities;
+				return (
+					<Box
+						key={weekdays[i]}
+						disableRipple
+						component={Button}
+						variant={!cardAvailabilities.includes(day) ? "outlined" : "contained"}
+						size="small"
+						disabled={!cardAvailabilities.includes(day)}
+					>
+						{day}
+					</Box>
+				);
+			})}
+		</ButtonGroup>
+	);
+};
+
+export default CardAvailabilities;
