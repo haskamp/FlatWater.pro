6bab08b6d8412616ca8f57622f2924493f90399f
Lennard Haskamp <haskamp.lennard@gmail.com>
2022-02-05 13:33:24 +0100

    chore: store refactored

--- src/ions/store/index.jsx
+++ src/ions/store/index.jsx
@@ -1,19 +1,28 @@
-import { v4 as uuid } from "uuid";
 import create from "zustand";
+import { v4 as uuid } from "uuid";
+import produce from "immer";
 
 const useStore = create(set => {
 	return {
 		cards: [],
 		setCard: formData =>
-			set(state => ({
-				cards: [
-					...state.cards,
-					{
+			set(
+				produce(state => {
+					state.cards.push({
 						...formData,
+						isExpanded: false,
 						id: uuid(),
-					},
-				],
-			})),
+					});
+				})
+			),
+		setExpanded: index =>
+			set(
+				produce(state => {
+					state.cards[index].isExpanded = !state.cards[index].isExpanded;
+				})
+			),
+		submitStatus: false,
+		setSubmitStatus: status => set({ submitStatus: status }),
 	};
 });
 
