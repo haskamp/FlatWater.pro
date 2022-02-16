3ed9f3d15f4dd09a49a5c4f92f7c904909acbffd
Lennard Haskamp <haskamp.lennard@gmail.com>
2022-02-16 11:48:43 +0100

    feat: save status auth with github

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,8 +1,13 @@
 import Head from "next/head";
 import React from "react";
 import Layout from "/src/organisms/layout";
+import { useSession, signIn, signOut } from "next-auth/react";
+import { Button } from "@mui/material";
+import { GitHub, Image } from "@mui/icons-material";
 
 const Page = () => {
+	const { data: session } = useSession();
+	console.log(session);
 	return (
 		<Layout>
 			<Head>
@@ -10,6 +15,28 @@ const Page = () => {
 				<meta name="description " content="This is my Capstone Project" />
 			</Head>
 			<h1>Homepage</h1>
+			{session ? (
+				<div>
+					<Image width="100px" src={session.user.image} alt={session.user.name} />
+					<h2>{session.user.name}</h2>
+					<Button
+						onClick={() => {
+							signOut("github");
+						}}
+					>
+						LogOut
+					</Button>
+				</div>
+			) : (
+				<Button
+					startIcon={<GitHub />}
+					onClick={() => {
+						signIn("github");
+					}}
+				>
+					LogIn with GitHub
+				</Button>
+			)}
 		</Layout>
 	);
 };
