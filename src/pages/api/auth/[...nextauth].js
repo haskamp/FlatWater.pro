ea6acf93d790c06cfa5d66d227b65490de66b240
Lennard Haskamp <haskamp.lennard@gmail.com>
2022-02-18 14:57:33 +0100

    feat: google login integrated

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,7 +1,8 @@
 import NextAuth from "next-auth";
-import GithubProvider from "next-auth/providers/github";
 import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
 import clientPromise from "/server/database/adapter/mongodb.js";
+import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
 
 export default NextAuth({
 	// Configure one or more authentication providers
@@ -10,6 +11,10 @@ export default NextAuth({
 		GithubProvider({
 			clientId: process.env.GITHUB_CLIENT_ID,
 			clientSecret: process.env.GITHUB_CLIENT_SECRET,
+		}),
+		GoogleProvider({
+			clientId: process.env.GOOGlE_CLIENT_ID,
+			clientSecret: process.env.GOOGLE_CLIENT_SECRET,
 		}),
 		// ...add more providers here
 	],
--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -3,7 +3,7 @@ import React from "react";
 import Layout from "/src/organisms/layout";
 import { useSession, signIn, signOut } from "next-auth/react";
 import { Button } from "@mui/material";
-import { GitHub, Image } from "@mui/icons-material";
+import { GitHub, Google } from "@mui/icons-material";
 
 const LogIn = () => {
 	// Sign In/Out Handle with data destructured as session
@@ -18,25 +18,36 @@ const LogIn = () => {
 			<h1>Homepage</h1>
 			{session ? (
 				<div>
-					<Image width="100px" src={session.user.image} alt={session.user.name} />
+					<img width="100px" src={session.user.image} alt={session.user.name} />
+					{/*add link for image in next config */}
 					<h2>{session.user.name}</h2>
 					<Button
 						onClick={() => {
-							signOut("github");
+							signOut("google");
 						}}
 					>
 						LogOut
 					</Button>
 				</div>
 			) : (
-				<Button
-					startIcon={<GitHub />}
-					onClick={() => {
-						signIn("github");
-					}}
-				>
-					LogIn with GitHub
-				</Button>
+				<div>
+					<Button
+						startIcon={<GitHub />}
+						onClick={() => {
+							signIn("github");
+						}}
+					>
+						LogIn with GitHub
+					</Button>
+					<Button
+						startIcon={<Google />}
+						onClick={() => {
+							signIn("google");
+						}}
+					>
+						LogIn with Google
+					</Button>
+				</div>
 			)}
 		</Layout>
 	);
